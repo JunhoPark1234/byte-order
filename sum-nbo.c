9ed4615d6dbb631e3fd6c6161c5c86733cd7bb37
JunhoPark1234 <145082589+JunhoPark1234@users.noreply.github.com>
2026-06-28 19:33:49 +0900

    Modified code
    Refactored file reading to use uint32_t directly and added a custom ntohl function for endianness conversion.

--- sum-nbo.c
+++ sum-nbo.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 #include <stdint.h>
 
+uint32_t my_ntohl(uint32_t n)
+{
+    uint32_t res1 = (n & 0xff000000) >> 24;
+    uint32_t res2 = (n & 0x00ff0000) >> 8;
+    uint32_t res3 = (n & 0x0000ff00) << 8;
+    uint32_t res4 = (n & 0x000000ff) << 24;
+
+    return res1 | res2 | res3 | res4;
+}
+
 int read_file(char *filename, uint32_t *number)
 {
     FILE *fp;
-    unsigned char buffer[4];
 
     fp = fopen(filename, "rb");
     if (fp == NULL) {
@@ -12,7 +21,7 @@ int read_file(char *filename, uint32_t *number)
         return 0;
     }
 
-    if (fread(buffer, 1, 4, fp) != 4) {
+    if (fread(number, sizeof(uint32_t), 1, fp) != 1) {
         printf("Error: file %s is smaller than 4 bytes\n", filename);
         fclose(fp);
         return 0;
@@ -20,10 +29,7 @@ int read_file(char *filename, uint32_t *number)
 
     fclose(fp);
 
-    *number = ((uint32_t)buffer[0] << 24) |
-              ((uint32_t)buffer[1] << 16) |
-              ((uint32_t)buffer[2] << 8)  |
-              (uint32_t)buffer[3];
+    *number = my_ntohl(*number);
 
     return 1;
 }
@@ -35,13 +41,14 @@ int main(int argc, char *argv[])
     uint32_t sum = 0;
 
     if (argc < 2) {
-        printf("usage : sum-nbo <file1> [<file2>...]\n");
-        return 0;
+        printf("syntax : sum-nbo <file1> [<file2>...]\n");
+        printf("sample : sum-nbo a.bin b.bin c.bin\n");
+        return 1;
     }
 
     for (i = 0; i < argc - 1; i++) {
         if (read_file(argv[i + 1], &numbers[i]) == 0) {
-            return 0;
+            return 1;
         }
     }
 
@@ -49,8 +56,9 @@ int main(int argc, char *argv[])
         printf("%u(0x%08x)", numbers[i], numbers[i]);
         sum += numbers[i];
 
-        if (i != argc - 2)
+        if (i != argc - 2) {
             printf(" + ");
+        }
     }
 
     printf(" = %u(0x%08x)\n", sum, sum);
